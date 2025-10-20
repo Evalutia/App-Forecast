bdce0f5903fdf52aa9ced2b3ed51af48692d120e
Santiago Dante <santiagodante4@gmail.com>
2025-10-20 17:36:32 -0300

    Configuracion de 'Ofelia' para la programacion diaria del job

---
+++ services/etl/run_ofelia.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+Y=$(date -d "yesterday" +%d/%m/%Y)
+
+exec /opt/pentaho/data-integration/kitchen.sh \
+  "-file=/app/services/etl/job_etl_diario.kjb" -level=Basic \
+  "-param:WS_URL=http://200.125.29.194:81" "-param:DATE_FMT=dmy" \
+  "-param:ID_EMPRESA=1" "-param:S_DEPOSITOS=1,5" "-param:GROUPS=201" \
+  "-param:MYSQL_HOST=mysql" "-param:MYSQL_DB=evalutia" \
+  "-param:MYSQL_USER=evalutia" "-param:MYSQL_PASSWORD=evalutia" "-param:MYSQL_PORT=3306" \
+  "-param:PREDICT_PERIODS=6" "-param:PREDICT_MODEL_SET=classic" "-param:PREDICT_VERSION=mvp-001" \
+  "-param:FORCE_START=$Y" "-param:FORCE_END=$Y"
